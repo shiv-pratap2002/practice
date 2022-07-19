8ad89009670708af39d643b87ce5c1cf5d1a6999
shiv-pratap2002 <83550179+shiv-pratap2002@users.noreply.github.com>
2022-07-19 13:18:17 +0530

    Update fastest_sort_ik.cpp

--- fastest_sort_ik.cpp
+++ fastest_sort_ik.cpp
@@ -1,3 +1,9 @@
+// This is the fastest sort I learnt in my studies, 
+//this is Quick Sort that uses Divide and Conquer Methodology
+//to solve the sorting problem. Most of the in-built functions 
+//in famous languages like python, use this in sorting due to its 
+//relative speed as compared to other Sorting Algorithms
+
 #include<bits/stdc++.h>
 
 using namespace std;
